3589e8f28fc81371c9bd8b171a476f517d3fcf10
jerry <jerry19971228@gmail.com>
2022-12-26 10:41:25 +0800

    建立all()

--- api/base.php
+++ api/base.php
@@ -16,19 +16,33 @@ class DB{
     public function find($id){
         $sql="select * from $this->table";
         if(is_array($id)){
-            foreach($id as $key => $value){
-                $tmp[]= "`$key`='$value'";
-            }
+            $tmp=$this->arrayToSqlArray($id);
 
             $sql = $sql . " where " . join(" && ",$tmp);
 
         }else{
-            $sql=$sql . " where `id` = '$id'";
+            $sql = $sql . " where `id` = '$id'";
         }
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-    public function all(){
+    public function all(...$arg){
+        $sql="select * from $this->table";
 
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $tmp=$this->arrayToSqlArray($arg[0]);
+
+                $sql=$sql . " where " . join(" && ",$tmp);
+            }else{
+                $sql = $sql . $arg[0];
+            }
+        }
+        
+        if(isset($arg[1])){
+            $sql = $sql . $arg[1];
+        }
+
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
     public function save(){
 
@@ -50,7 +64,14 @@ class DB{
     }
     public function avg(){
 
-    }   
+    }
+    
+    private function arrayToSqlArray($array){
+        foreach($array as $key => $value){
+            $tmp[]= "`$key`='$value'";
+        }
+        return $tmp;
+    }
 }
 function dd(){
 
@@ -63,6 +84,5 @@ function q(){
 }
 
 $db=new DB('bottom');
-$bot=$db->find(1);
+$bot=$db->all();
 print_r($bot);
-?>
