1794aaa2ad4182d225594e5daafc38e6df8d1c69
jerry <jerry19971228@gmail.com>
2022-12-29 08:55:20 +0800

    在前端頁面顯示出設為顯示的標題圖片在標題區

--- back.php
+++ back.php
@@ -24,7 +24,7 @@ include "./api/base.php";
 	
 	<div id="main">
 		<a title="" href="index.php">
-			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div>
+			<div class="ti" style="background:url(&#39;upload/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div>
 			<!--標題-->
 		</a>
 		<div id="ms">
