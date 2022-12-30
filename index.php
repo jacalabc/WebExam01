b1a43f066069abeacd97e54b1c833fa61dc25c91
jerry <jerry19971228@gmail.com>
2022-12-30 16:23:24 +0800

    完成更多消息彈出視窗功能

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include "./api/base.php";
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
@@ -21,9 +22,9 @@ include "./api/base.php";
 			<div id="cvr" style="position:absolute; width:99%; height:100%; margin:auto; z-index:9898;"></div>
 		</div>
 	</div>
-	
+	<iframe style="display:none;" name="back" id="back"></iframe>
 	<div id="main">
-	<a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
+		<a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
 			<div class="ti" style="background:url(&#39;upload/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div>
 			<!--標題-->
 		</a>
@@ -34,7 +35,7 @@ include "./api/base.php";
 					<span class="t botli">主選單區</span>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 : <?=$Total->find(1)['total'];?></span>
+					<span class="t">進站總人數 :<?=$Total->find(1)['total'];?> </span>
 				</div>
 			</div>
 			<?php
@@ -47,22 +48,7 @@ include "./api/base.php";
 				}
 			 
 			 ;?>
-			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
-			<script>
-				$(".sswww").hover(
-					function() {
-						$("#alt").html("" + $(this).children(".all").html() + "").css({
-							"top": $(this).offset().top - 50
-						})
-						$("#alt").show()
-					}
-				)
-				$(".sswww").mouseout(
-					function() {
-						$("#alt").hide()
-					}
-				)
-			</script>
+			
 			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 				<!--右邊-->
 				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
@@ -93,11 +79,7 @@ include "./api/base.php";
 		</div>
 		<div style="clear:both;"></div>
 		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;">
-				<?php
-					echo $Bottom->find(1)['bottom'];
-				?>
-			</span>
+			<span class="t" style="line-height:123px;"><?=$Bottom->find(1)['bottom'];?></span>
 		</div>
 	</div>
 
